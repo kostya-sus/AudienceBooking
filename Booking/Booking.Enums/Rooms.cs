583d9e95a2a9fe834473af32b24df9cb93b13a1a
Andrii Lysenko <andrewkvlp@gmail.com>
2016-11-17 13:57:50 +0200

    Added Booking.Enums projects.

---
+++ Booking/Booking.Enums/Rooms.cs
@@ -0,0 +1,15 @@
+﻿namespace Booking.Enums
+{
+    public enum Rooms
+    {
+        HrOffice,
+        EinsteinClassroom,
+        InfoCenter,
+        WebAndMarketing1,
+        WebAndMarketing2,
+        WebAndMarketing3,
+        English,
+        TeslaClassroom,
+        NewtonClassroom
+    }
+}
