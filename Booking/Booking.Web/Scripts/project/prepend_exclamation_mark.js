96675796bf100d8eb1551e443a2b504a73aef0b8
Andrii Lysenko <andrewkvlp@gmail.com>
2016-11-23 15:32:28 +0200

    Added script for validation message exclamation mark adding.

---
+++ Booking/Booking.Web/Scripts/project/prepend_exclamation_mark.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    validation_icon = '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> ';
+    $(".prepend-exclamation-mark").each(function (i, val) {
+        var $element = $(val);
+        var innerHtml = $element.html();
+        $element.html(validation_icon + innerHtml);
+    });
+});
