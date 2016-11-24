bb12d27a945a912a5d33f156a46d73699e11004d
Andrii Lysenko <andrewkvlp@gmail.com>
2016-11-25 01:21:13 +0200

    Added font-awesome to content.

---
+++ Booking/Booking.Web/Scripts/project/login.js
@@ -0,0 +1,24 @@
+﻿function prependExclamationMarkToValidationMessages() {
+    var validationIcon = '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> ';
+    $(".prepend-exclamation-mark")
+        .each(function(i, val) {
+            var $element = $(val);
+            if ($element.is(":empty")) {
+                return;
+            }
+
+            var innerHtml = $element.html();
+            $element.html(validationIcon + innerHtml);
+        });
+}
+
+$(document)
+    .ready(function() {
+        var form = $("form");
+        var validator = form.data("validator");
+
+        validator.settings.showErrors = function() {
+            $('input[type="submit"]').prop('disabled', (this.numberOfInvalids() === 0));
+            this.defaultShowErrors();
+        };
+    });
