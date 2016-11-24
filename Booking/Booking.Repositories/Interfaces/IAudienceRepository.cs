06999deda113ff16236c7853229aee58dcd887d5
Andrii Lysenko <andrewkvlp@gmail.com>
2016-11-24 22:15:26 +0200

    Added repositories and unit of work interfaces.

---
+++ Booking/Booking.Repositories/Interfaces/IAudienceRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Booking.Models;
+
+namespace Booking.Repositories.Interfaces
+{
+    public interface IAudienceRepository : IDisposable
+    {
+        IQueryable<Audience> GetAllAudiences();
+
+        Audience GetAudienceById(int id);
+
+        void UpdateAudience(Audience audience);
+
+        void Save();
+    }
+}
