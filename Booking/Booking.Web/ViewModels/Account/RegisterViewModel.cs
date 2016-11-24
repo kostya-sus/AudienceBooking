43342a1e3df008dca854b8b11057a1159152dd97
Andrii Lysenko <andrewkvlp@gmail.com>
2016-11-24 16:04:00 +0200

    Few changes in Register vm.

--- Booking/Booking.Web/ViewModels/Account/RegisterViewModel.cs
+++ Booking/Booking.Web/ViewModels/Account/RegisterViewModel.cs
@@ -4,25 +4,21 @@ namespace Booking.Web.ViewModels.Account
 {
     public class RegisterViewModel
     {
-        [Required(ErrorMessage = " ")]
-        [RegularExpression(@"^[a-zA-Zа-яА-Я']+\s[a-zA-Zа-яА-Я-' ]+$", ErrorMessage = " ")]
-        [Display(Name = "UserName")]
+        [Required]
+        [RegularExpression(@"^([A-ZА-ЯЁЇІЄ][a-zа-яёіїє']+ -)+[A-ZА-ЯЁЇІЄ][a-zа-яёіїє']+$",
+             ErrorMessage = "Неправильный формат имени и фамилии.")]
         public string UserName { get; set; }
 
-        [Required(ErrorMessage = " ")]
+        [Required]
         [EmailAddress(ErrorMessage = "Неверный адрес электронной почты")]
-        [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = " ")]
+        [Required]
         [StringLength(15, ErrorMessage = "Пароль должен содержать 8-15 символов", MinimumLength = 8)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
         public string Password { get; set; }
 
-        [Required(ErrorMessage = " ")]
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Required]
         [Compare("Password", ErrorMessage = "Пароли не совпадают")]
         public string ConfirmPassword { get; set; }
     }
