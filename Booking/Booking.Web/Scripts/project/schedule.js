88ab7e245929a27c9ed73b8d50e699ecc91a0a85
Andrii Lysenko <andrewkvlp@gmail.com>
2016-11-27 18:36:38 +0200

    Made slider draggable.

--- Booking/Booking.Web/Scripts/project/schedule.js
+++ Booking/Booking.Web/Scripts/project/schedule.js
@@ -1,1 +1,11 @@
-﻿
+﻿$(document)
+    .ready(function() {
+        $("#slider-draggable")
+            .draggable({
+                axis: "x",
+                containment: "parent",
+                drag:function(event, ui) {
+                    console.log(ui.position.left);
+                }
+            });
+    });
