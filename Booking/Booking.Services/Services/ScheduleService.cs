caa60c753b4e428274e38bb7bd04f5cf2323ec5f
Kabawa <deymosmile@ukr.net>
2016-11-29 21:49:08 +0200

    implement schedule-service

--- Booking/Booking.Services/Services/ScheduleService.cs
+++ Booking/Booking.Services/Services/ScheduleService.cs
@@ -7,33 +7,40 @@ using System.Threading.Tasks;
 using Booking.Enums;
 using Booking.Models;
 using Booking.Repositories;
+using Booking.Repositories.Interfaces;
+using Booking.Repositories.Repositories;
 using Booking.Services.Interfaces;
 
 namespace Booking.Services.Services
 {
     public class ScheduleService : IScheduleService
     {
-        private readonly UnitOfWork _unitOfWork = new UnitOfWork();
+        private readonly IEventRepository _eventRepository;
+
+        public ScheduleService(IEventRepository eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+
         public IEnumerable<Event> GetEventsByDay(DateTime day)
         {
-            var res = _unitOfWork.EventRepository.GetAllEvents();
-            res =  res.Where(x => x.EventDateTime == day);
+            var res = _eventRepository.GetAllEvents();
+            res = res.Where(x => x.EventDateTime.Date == day.Date);
             return res;
         }
 
         public IEnumerable<Event> GetEventsByAudience(int audienceId, DateTime @from, DateTime to)
         {
-            var res = _unitOfWork.EventRepository.GetAllEvents();
-            var duration = to - @from;
-            
-           // res = res.Where(x => x.AudienceId == (AudiencesEnum) audienceId && x.EventDateTime <= @from && );
+            var res = _eventRepository.GetAllEvents();
+            res = res.Where( x => x.AudienceId == (AudiencesEnum) audienceId && x.EventDateTime <= @from &&
+                                                                x.EventDateTime.AddMinutes(x.Duration) < to);
             return res;
         }
 
         public IEnumerable<Event> GetEventsByAuthor(ApplicationUser author, DateTime day)
         {
-            var res = _unitOfWork.EventRepository.GetAllEvents();
-            res = res.Where(x => x.AuthorId == author.Id && x.EventDateTime == day);
+            var res = _eventRepository.GetAllEvents();
+            res = res.Where(x => x.AuthorId == author.Id && x.EventDateTime.Date == day.Date);
             return res;
         }
     }
