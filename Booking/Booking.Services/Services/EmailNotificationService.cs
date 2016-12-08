e376bccd4d243396fa32e049e8c624c88ebec274
Andrii Lysenko <andrewkvlp@gmail.com>
2016-12-08 22:25:52 +0200

    Added empty EmailNotificationService.

---
+++ Booking/Booking.Services/Services/EmailNotificationService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Booking.Models;
+using Booking.Services.Interfaces;
+
+namespace Booking.Services.Services
+{
+    public class EmailNotificationService : IEmailNotificationService
+    {
+        public void AccountRegisteredNotification(ApplicationUser user)
+        {
+        }
+
+        public void AccountRemovedNotification(ApplicationUser user)
+        {
+        }
+
+        public void EventCancelledNotification(Event eventEntity)
+        {
+        }
+
+        public void EventCancelledAuthorNotification(Event eventEntity)
+        {
+        }
+
+        public void RemovedFromParticipantsListNotification(string email, Event eventEntity)
+        {
+        }
+
+        public void EventJoinedNotification(string email, Event eventEntity)
+        {
+        }
+
+        public void EventEditedNotification(Event newEvent, Event oldEvent)
+        {
+        }
+
+        public void EventEditedAuthorNotification(Event newEvent, Event oldEvent)
+        {
+        }
+
+        public void SendFeedbackToAdmins(string name, string surname, string email, string message)
+        {
+        }
+    }
+}
