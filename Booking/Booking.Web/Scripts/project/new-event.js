23dc9cd2639de8322f95dd50da9b51e5b4933da8
Sergey-Gaydaenko <deymosmile@ukr.net>
2016-12-08 15:14:39 +0200

    working with custom datetimePicker

--- Booking/Booking.Web/Scripts/project/new-event.js
+++ Booking/Booking.Web/Scripts/project/new-event.js
@@ -22,4 +22,56 @@ $(document).ready(function () {
 });
 
 
+function incrementDayValue(id) {
+    var value = parseInt(document.getElementById(id.toString()).value);
+    value = isNaN(value) ? 1 : value;
+    if (value !== 31) {
+        value++;
+    }
+    document.getElementById(id.toString()).value = value;
+}
 
+function decrementDayValue(id) {
+    var value = parseInt(document.getElementById(id.toString()).value);
+    value = isNaN(value) ? 1 : value;
+    if (value !== 1) {
+        value--;
+    }
+    document.getElementById(id.toString()).value = value;
+}
+
+function incrementHourValue(id) {
+    var value = parseInt(document.getElementById(id.toString()).value);
+    value = isNaN(value) ? 12 : value;
+    if (value !== 19) {
+        value++;
+    }
+    document.getElementById(id.toString()).value = value;
+}
+
+function decrementHourValue(id) {
+    var value = parseInt(document.getElementById(id.toString()).value);
+    value = isNaN(value) ? 12 : value;
+    if (value !== 12) {
+        value--;
+    }
+    document.getElementById(id.toString()).value = value;
+}
+
+function incrementMinuteValue(id) {
+    var value = parseInt(document.getElementById(id.toString()).value);
+    value = isNaN(value) ? 30 : value;
+    if (value !== 55) {
+        value+=5;
+    }
+    document.getElementById(id.toString()).value = value;
+}
+
+function decrementMinuteValue(id) {
+    var value = parseInt(document.getElementById(id.toString()).value);
+    value = isNaN(value) ? 30 : value;
+    if (value !== 0) {
+        value-=5;
+    }
+    document.getElementById(id.toString()).value = value;
+}
