01858c5ae59609a078ea34c5aed3b090d5052410
Andrii Lysenko <andrewkvlp@gmail.com>
2016-12-08 15:26:46 +0200

    Added audience info partial view loading.

--- Booking/Booking.Web/Scripts/project/room-map.js
+++ Booking/Booking.Web/Scripts/project/room-map.js
@@ -1,11 +1,17 @@
 ﻿function onRoomProxyMouseEnter() {
-    var targetDivId = $(this).data("room-target");
-    $("#" + targetDivId).addClass("room-proxy-hover");
+    var targetDivId = "#" + $(this).data("room-target");
+    $(targetDivId).addClass("room-proxy-hover");
+    var audienceId = $(targetDivId).data("audience-id");
+    var audienceInfoUrl = $("#get-audience-info-url").val();
+    var $div = $("<div id='audience-info'></div>");
+    $("#room-map-container").append($div);
+    $div.load(audienceInfoUrl + "?audienceId=" + audienceId);
 }
 
 function onRoomProxyMouseLeave() {
     var targetDivId = $(this).data("room-target");
     $("#" + targetDivId).removeClass("room-proxy-hover");
+    $("#audience-info").remove();
 }
 
 $(document)
