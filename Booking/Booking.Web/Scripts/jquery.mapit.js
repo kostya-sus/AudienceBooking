6983514f09825d415e296013f5f59041bab5eecb
Andrii Lysenko <andrewkvlp@gmail.com>
2016-12-09 11:11:43 +0200

    Fixed google maps issues.

--- Booking/Booking.Web/Scripts/jquery.mapit.js
+++ Booking/Booking.Web/Scripts/jquery.mapit.js
@@ -21,9 +21,6 @@
 	*/
 
 
-document.write('<scr' + 'ipt type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false" ></scr' + 'ipt>');
-
-
 
 
 ;(function($, window, undefined) {
