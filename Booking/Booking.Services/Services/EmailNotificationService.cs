be90072fca31141cb22aa12e457341bc8d8ed515
Kabawa <deymosmile@ukr.net>
2016-12-15 19:39:51 +0200

    added emailRegisteredNotification template, implement method to send message after user registered

--- Booking/Booking.Services/Services/EmailNotificationService.cs
+++ Booking/Booking.Services/Services/EmailNotificationService.cs
@@ -1,13 +1,57 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 using Booking.Models;
 using Booking.Services.Interfaces;
+using RazorEngine.Templating;
 
 namespace Booking.Services.Services
 {
     public class EmailNotificationService : IEmailNotificationService
     {
+        private static readonly string TemplateFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "EmailTemplates");
         public void AccountRegisteredNotification(ApplicationUser user)
         {
+            var emailTemplatePath = Path.Combine(TemplateFolderPath, "AccountRegisteredNotification.cshtml");
+           
+            var modelEmail = new {user.Email, Name = user.UserName};
+
+            var templateService = new TemplateService();
+            var emailHtmlBody = templateService.Parse(File.ReadAllText(emailTemplatePath), modelEmail, null, null);
+
+            // Send the email
+            var email = new MailMessage()
+            {
+                Body = emailHtmlBody,
+                IsBodyHtml = true,
+                Subject = "Registered to softheme-booking.azurewebsites.net"
+            };
+
+            email.To.Add(new MailAddress(modelEmail.Email, modelEmail.Name));
+
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                Credentials = new NetworkCredential("audiencebookingtest@gmail.com", "Qwer123!"),
+                EnableSsl = true
+            };
+
+            try
+            {
+                smtp.Send(email);
+            }
+            catch (Exception ex)
+            {
+                var smtpEx = ex as SmtpException;
+                if (smtpEx != null)
+                {
+                    var error = smtpEx.StatusCode;
+                }
+
+            }
         }
 
         public void AccountRemovedNotification(ApplicationUser user)
--- Booking/Booking.Web/Controllers/AccountController.cs
+++ Booking/Booking.Web/Controllers/AccountController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Booking.Models;
+using Booking.Services.Interfaces;
+using Booking.Services.Services;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
@@ -18,9 +20,10 @@ namespace Booking.Web.Controllers
     {
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
-
+        private IEmailNotificationService _emailNotificationService;
         public AccountController()
         {
+            _emailNotificationService = new EmailNotificationService();
         }
 
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
@@ -154,13 +157,13 @@ namespace Booking.Web.Controllers
                     if (result.Succeeded)
                     {
                         await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
-
+                        
                         // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                         // Send an email with this link
                         // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                         // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                         // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
+                        _emailNotificationService.AccountRegisteredNotification(user);
                         return RedirectToAction("Index", "Home");
                     }
                 }
