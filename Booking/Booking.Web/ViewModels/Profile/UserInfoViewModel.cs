faaead95bc45ab63700ceb214510a959c49d16ab
kostya-sus <kostya-sus@yandex.ru>
2016-12-15 02:25:48 +0200

    small edit of partial profile page

--- Booking/Booking.Web/ViewModels/Profile/UserInfoViewModel.cs
+++ Booking/Booking.Web/ViewModels/Profile/UserInfoViewModel.cs
@@ -1,9 +1,19 @@
-﻿namespace Booking.Web.ViewModels.Profile
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Booking.Web.ViewModels.Profile
 {
     public class UserInfoViewModel
     {
+        [Required(ErrorMessageResourceType = typeof(Localization.Localization),
+        ErrorMessageResourceName = "ValidationMessage_Required")]
+        [RegularExpression(@"^([A-ZА-ЯЁЇІЄ][a-zа-яёіїє']+)([ -][A-ZА-ЯЁЇІЄ][a-zа-яёіїє']+)+$",
+        ErrorMessageResourceType = typeof(Localization.Localization),
+        ErrorMessageResourceName = "RegisterViewModel_UserName_ValidationMessage")]
         public string Name { get; set; }
-
+        [Required(ErrorMessageResourceType = typeof(Localization.Localization),
+          ErrorMessageResourceName = "ValidationMessage_Required")]
+        [EmailAddress(ErrorMessageResourceType = typeof(Localization.Localization),
+          ErrorMessageResourceName = "RegisterViewModel_Email_ValidationMessage")]
         public string Email { get; set; }
 
         public int ActiveEventsCount { get; set; }
