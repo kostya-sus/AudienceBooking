d3a5dca952e017b30300835e3868547d860b352b
Andrii Lysenko <andrewkvlp@gmail.com>
2016-12-18 18:44:35 +0200

    Moved GetDaySchedule action from Home controller to new Schedule controller.

--- Booking/Booking.Web/Controllers/HomeController.cs
+++ Booking/Booking.Web/Controllers/HomeController.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using AutoMapper;
-using Booking.Enums;
 using Booking.Repositories;
-using Booking.Repositories.Repositories;
 using Booking.Services.Interfaces;
 using Booking.Services.Services;
 using Booking.Web.Helpers;
-using Booking.Web.ViewModels;
-using Booking.Web.ViewModels.Audience;
 using Booking.Web.ViewModels.AudienceMap;
 using Booking.Web.ViewModels.Home;
 
@@ -20,15 +12,11 @@ namespace Booking.Web.Controllers
     [HandleException]
     public class HomeController : Controller
     {
-        private readonly IAudienceService _audienceService;
-        private readonly IScheduleService _scheduleService;
         private readonly IAudienceMapService _audienceMapService;
 
         public HomeController()
         {
             var uof = new UnitOfWork();
-            _audienceService = new AudienceService(uof);
-            _scheduleService = new ScheduleService(uof.EventRepository);
             _audienceMapService = new AudienceMapService(uof);
         }
 
@@ -45,22 +33,6 @@ namespace Booking.Web.Controllers
             };
 
             return View(viewModel);
-        }
-
-        [HttpGet]
-        public ActionResult GetDaySchedule(DateTime date)
-        {
-            var events = _scheduleService.GetEventsByDay(date, AudienceMapSelector.AudienceMapId);
-            var viewModel = Mapper.Map<DayScheduleViewModel>(events);
-            viewModel.BookingHourStart = (int) BookingHoursBoundsEnum.Lower;
-            viewModel.BookingHourEnd = (int) BookingHoursBoundsEnum.Upper;
-
-            var audienceMap = _audienceMapService.GetAudienceMap(AudienceMapSelector.AudienceMapId);
-            var availableAudiences = audienceMap.Audiences.Where(a => a.IsBookingAvailable)
-                .Select(a => new ScheduleRowName {Id = a.Id, Name = a.Name});
-            viewModel.AvailableAudiences = availableAudiences;
-
-            return Json(viewModel, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ Booking/Booking.Web/Controllers/ScheduleController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using AutoMapper;
+using Booking.Enums;
+using Booking.Repositories;
+using Booking.Services.Interfaces;
+using Booking.Services.Services;
+using Booking.Web.Helpers;
+using Booking.Web.ViewModels.Home;
+
+namespace Booking.Web.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        private readonly IScheduleService _scheduleService;
+        private readonly IAudienceMapService _audienceMapService;
+
+        public ScheduleController()
+        {
+            var uof = new UnitOfWork();
+            _scheduleService = new ScheduleService(uof.EventRepository);
+            _audienceMapService = new AudienceMapService(uof);
+        }
+
+        [HttpGet]
+        public ActionResult GetDaySchedule(DateTime date)
+        {
+            var events = _scheduleService.GetEventsByDay(date, AudienceMapSelector.AudienceMapId);
+            var viewModel = Mapper.Map<DayScheduleViewModel>(events);
+            viewModel.BookingHourStart = (int)BookingHoursBoundsEnum.Lower;
+            viewModel.BookingHourEnd = (int)BookingHoursBoundsEnum.Upper;
+
+            var audienceMap = _audienceMapService.GetAudienceMap(AudienceMapSelector.AudienceMapId);
+            var availableAudiences = audienceMap.Audiences.Where(a => a.IsBookingAvailable)
+                .Select(a => new ScheduleRowName { Id = a.Id, Name = a.Name });
+            viewModel.AvailableAudiences = availableAudiences;
+
+            return Json(viewModel, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
